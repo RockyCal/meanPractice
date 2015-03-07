a6cf3fb69ab73b5d76232b4f6563b2866b379123
Raquel <calderon.rocky@gmail.com>
2015-03-07 11:54:32 -0800

    added nerd route

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,33 @@
-module.exports = function(app) {
+// app/routes/js
 
-	// server routes ===========================================================
-	// handle things like api calls
-	// authentication routes
+// grab the nerd model we just created
 
-	// frontend routes =========================================================
-	// route to handle all angular requests
-	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html');
-	});
+var Nerd = require('./models/nerd');
 
-};
+
+	module.exports = function(app) {
+
+		// server routes ===========================================================
+		// handle things like api calls
+		// authentication routes
+
+
+		// sample api route
+		app.get('/api/nerds', function(req, res) {
+			Nerd.find(function(err, nerds) {
+				// if there is an error retrieving, send error
+				//nothing after res.send(err) will execture
+				if (err)
+					res.send(err);
+
+				res.json(nerds); // return all nerds in json format
+		    });
+		});
+
+		// frontend routes =========================================================
+		// route to handle all angular requests
+		app.get('*', function(req, res) {
+			res.sendfile('./public/views/index.html');
+		});
+
+	};
